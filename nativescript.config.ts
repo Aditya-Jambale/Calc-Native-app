f067fa030e219710babb75a780aa696060ba6a93
Aditya Jambale <96608515+Aditya-Jambale@users.noreply.github.com>
2024-12-26 20:15:56 +0530

    Add files via upload

---
+++ nativescript.config.ts
@@ -0,0 +1,13 @@
+import { NativeScriptConfig } from '@nativescript/core';
+
+export default {
+  id: 'org.nativescript.app',
+  appPath: 'src',
+  appResourcesPath: '../../tools/assets/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none'
+  },
+  appId: 'com.adityajambale.calculator',
+  displayName: 'Calc by Aditya Jambale'
+} as NativeScriptConfig;
